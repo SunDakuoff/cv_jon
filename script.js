0488055132193b9ae7500ea0574508c697cba8f1
gh <mgngn@gmail.com>
2025-12-04 15:47:23 +0100

    index v4

---
+++ script.js
@@ -0,0 +1,9 @@
+const togglebtn = document.getElementById("darkmode-btn");
+togglebtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+   
+const togglebtn2 = document.getElementById("visitcard2");
+togglebtn.addEventListener("click", () => {
+    document.body.classList.toggle2("visitcardbtn");
+});
