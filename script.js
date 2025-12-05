2da6cc2c4d2e36094e6efdc7d1e07515d9f2ae67
gh <mgngn@gmail.com>
2025-12-05 09:25:05 +0100

    index v4.21

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const togglebtn = document.getElementById("darkmode-btn");
 togglebtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
-   
+
 const togglebtn2 = document.getElementById("visitcard2");
 togglebtn.addEventListener("click", () => {
     document.body.classList.toggle2("visitcardbtn");
